11c08ac729290dc53249a57810be4f93b9821e5a
srisathyamamidala <77813332+srisathyamamidala@users.noreply.github.com>
2022-01-28 13:55:29 -0600

    output generated by running the python sort and reduce

---
+++ 01sort.py
@@ -0,0 +1,14 @@
+# Sri Sathya Mamidala
+
+
+n = open("output.txt","r")  # open file, read-only
+s = open("s.txt", "w") # open file, write
+lines = n.readlines()
+lines.sort()
+
+for line in lines:
+	s.write(line)
+n.close()
+s.close()
+
+
